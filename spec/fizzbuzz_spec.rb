8c627fe697661a18c7092573f90151d165e3bee2
Margarida Martelo <margmartelo@gmail.com>
2020-10-20 16:27:21 +0100

    Adding Third and Fourth Tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,5 +10,12 @@ describe "fizzbuzz" do
   it 'returns "buzz" for the number 5' do
     expect(5.fizzbuzz).to eq 'buzz'
   end
-  
+
+  it 'returns "fizz" for numbers that are multiples of 3' do
+    expect(6.fizzbuzz).to eq 'fizz'
+  end
+
+  it 'returns "buzz" for numbers that are multiples of 5' do
+    expect(10.fizzbuzz).to eq 'buzz'
+  end
 end

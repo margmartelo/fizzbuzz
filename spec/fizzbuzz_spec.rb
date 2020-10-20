bea96392495b4c248be1b2da906bee5e9897ae79
Margarida Martelo <margmartelo@gmail.com>
2020-10-20 16:15:49 +0100

    Adding Second Test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,9 +2,13 @@ require 'fizzbuzz'
 
 describe "fizzbuzz" do
   # First test: returns 'fizz' for number 3
-  it 'retuns "fizz" for the number 3' do
+  it 'returns "fizz" for the number 3' do
     expect(3.fizzbuzz).to eq 'fizz'
   end
 
-
+  # Second test: returns 'buzz' for number 5
+  it 'returns "buzz" for the number 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+  
 end

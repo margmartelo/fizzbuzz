927e8e78ce9d76a01c2e670d5c9c2533e79077b1
CodeZeus-dev <constantine.akritides@gmail.com>
2020-10-20 16:36:49 +0100

    Modified the fizzbuzz.rb script so that it returns "fizzbuzz" for multiples of both 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,8 @@
 class Integer
   def fizzbuzz
-    if self % 3 == 0
+    if self % 3 == 0 && self % 5 == 0
+      return "fizzbuzz"
+    elsif self % 3 == 0
       return "fizz"
     elsif self % 5 == 0
       return "buzz"
